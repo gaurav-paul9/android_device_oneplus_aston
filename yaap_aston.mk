21ec8dd2fe31cae1037c4ec67a773d6ea1255275
gaurav-paul9 <154791745+gaurav-paul9@users.noreply.github.com>
2025-10-19 17:42:16 +0000

    aston: Initial YAAP Bringup

--- yaap_aston.mk
+++ yaap_aston.mk
@@ -14,18 +14,19 @@ $(call inherit-product, device/oneplus/aston/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_aston
+PRODUCT_NAME := yaap_aston
 PRODUCT_DEVICE := aston
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := CPH2609
 
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2780
+TARGET_SCREEN_WIDTH := 1264
 
+# Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 15 AP3A.240617.008 1758859159691 release-keys" \
-    BuildFingerprint=OnePlus/CPH2609EEA/OP5D35L1:15/TP1A.220905.001/U.R4T3.1632d6d_9d6f90_9e012b:user/release-keys \
-    DeviceName=OP5D35L1 \
-    DeviceProduct=CPH2609 \
-    SystemDevice=OP5D35L1 \
-    SystemName=CPH2609
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_NAME=$(PRODUCT_SYSTEM_NAME)
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
