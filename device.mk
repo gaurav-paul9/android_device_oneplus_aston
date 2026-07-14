aa715537e5491bde243ef6540a248022ca0ef66c
Gaurav Paul <paulgaurav9@gmail.com>
2026-07-14 20:00:58 +0000

    Revert "aston: Use lineage-libperfmgr for DT2W"

--- device.mk
+++ device.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
     OPlusSystemUIResTarget
 
 # Power
+$(call soong_config_set,qtipower,mode_ext_lib,power-ext-oplus)
 $(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-aston)
 
 # Regional properties
