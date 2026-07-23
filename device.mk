ba8721242af4b16f6c031888d4218499636ef854
gaurav-paul9 <paulgaurav9@gmail.com>
2026-07-23 20:03:00 +0530

    Revert "lindroid: Add vendor_lindroid product integration"
    This reverts commit 6b0dd8cfabd83bbb264a2c8b03d22e57db5e0904.

--- device.mk
+++ device.mk
@@ -113,6 +113,3 @@ PRODUCT_PACKAGES += \
 
 # OPLUS Fusion Light Sensor
 $(call inherit-product-if-exists, vendor/oneplus/fusion/fusion-vendor.mk)
-
-# Lindroid: Linux on Droid
-$(call inherit-product, vendor/lindroid/lindroid.mk)
